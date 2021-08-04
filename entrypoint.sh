ffd676e3cb417ca41882c35ccf6d2321de5625f5
Chonwai <chonwaiun@gmail.com>
2021-08-04 22:12:10 +0800

    Update: Updated the DockerFile.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 echo python3 --version
-python3 generate.py --prefix='鋼筆，銀色，時尚，鋼製，熱賣。' --length=50 --nsamples=5 --model_path='model/model_wiki5_epoch1' --tokenizer_path='cache/vocab.txt'
+python3 generate.py --prefix='鋼筆，銀色，時尚，鋼製。' --length=50 --nsamples=5 --model_path='model/model_wiki5_epoch1' --tokenizer_path='cache/vocab.txt'
