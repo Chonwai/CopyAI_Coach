4922a0db456340d950b348d37ec12f85457ee3c8
Chonwai <chonwaiun@gmail.com>
2021-08-13 18:01:40 +0800

    Update: Updated the docker-compose setting.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!bin/bash
 echo python3 --version
-python3 generate.py --prefix='鋼筆，銀色，時尚，鋼製，屌你老母。' --length=50 --nsamples=5 --model_path='model/model_wiki100_epoch2' --tokenizer_path='cache/vocab.txt'
+echo ${BASH_ARGV[*]}
+
+# python3 generate.py --prefix='鋼筆，銀色，時尚，鋼製，熱賣。' --length=50 --nsamples=5 --model_path='model/model_wiki100_epoch7' --tokenizer_path='cache/vocab.txt'
+python3 generate.py ${BASH_ARGV[*]}
